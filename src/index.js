6a33ec56bd2983a27f6e05250920ed61ba96c60f
Moshik-laptop <moshiktm1994@gmail.com>
2024-04-15 03:31:03 +0300

    change navbar and add text liked and openers liked

--- src/index.js
+++ src/index.js
@@ -26,8 +26,8 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
 <Auth0Provider
-    domain={process.env.REACT_APP_AUTO_0_DOMAIN_LOCAL}
-    clientId={process.env.REACT_APP_AUTO_0_CLIENTID_LOCAL}
+    domain={process.env.REACT_APP_AUTO_0_DOMAIN_PRODUCTION}
+    clientId={process.env.REACT_APP_AUTO_0_CLIENTID_PRODUCTION}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
