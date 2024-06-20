491c834f58d405df0cf6d2c4fb09364b9ff23bdd
Moshik Ziyoni <moshik.ziyoni@outseer.com>
2024-06-20 12:42:09 +0300

    change colors+fontfamily

--- src/components/SpinnerHeart/HeartSpinner.js
+++ src/components/SpinnerHeart/HeartSpinner.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import './spinner.css'
+import './spinner2.css'
 function HeartSpinner() {
   return (
     <div>
     <p className='my_p'>Loading...</p>
-    <div class="spinner-heart" style={{fontStyle:'normal',fontFamily:'initial'}}>
+    {/* <div class="spinner-heart" style={{fontStyle:'normal',fontFamily:'initial'}}> */}
+    <div class="container">
+      <div class="preloader">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+{/* <div class="shadow">
+</div> */}
+</div>
+    {/* </div> */}
 
-    </div>
+
     </div> 
 
   )
